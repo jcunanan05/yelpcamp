f5f4e4020356edf48c5209ba7a7304b6f0ede260
Jonathan <jcunanan05@gmail.com>
2018-04-16 20:06:38 +0000

    Add Basic styling
    Added basic styling in campgrounds page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 let campgrounds = [
     {name: 'Salmon Creek', image:'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144396f8c97dafebb4_340.jpg'},
     {name: 'Granite Hill', image:'https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104497f3c971a3e5b3b9_340.jpg'},
+    {name: `Mountain Goat's Rest`, image:'https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg'},
+    {name: 'Salmon Creek', image:'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144396f8c97dafebb4_340.jpg'},
+    {name: 'Granite Hill', image:'https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104497f3c971a3e5b3b9_340.jpg'},
     {name: `Mountain Goat's Rest`, image:'https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg'}
   ];
   
