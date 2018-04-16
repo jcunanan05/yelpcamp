f8fc273b0a1c4d2d35afb6778fd00e8c588dd90d
Jonathan <jcunanan05@gmail.com>
2018-04-16 17:27:56 +0000

    Add body-parser and Add campground feature
    Added /campgrounds/new and a form

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 var express = require('express');
 var app = express();
-
-app.set('view engine', 'ejs')
-
-
-app.get('/', function(req, res) {
-  res.render('landing')
-});
-
-app.get('/campgrounds', function(req, res) {
-  var campgrounds = [
+var bodyParser = require('body-parser');
+let campgrounds = [
     {name: 'Salmon Creek', image:'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144396f8c97dafebb4_340.jpg'},
     {name: 'Granite Hill', image:'https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104497f3c971a3e5b3b9_340.jpg'},
     {name: `Mountain Goat's Rest`, image:'https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg'}
   ];
   
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+  res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res) {
+  //post route when creating a new campground
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name, image};
+  
+  campgrounds.push(newCampground);
+  
+  //redirect
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new');
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
